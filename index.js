de49b13ebfa373437fcf5a6261ae0b12041f620b
Jayon Kim <jpkim921@gmail.com>
2018-09-26 04:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random() * 20;
 const randomInteger = Math.floor(randomNumber);
-// const randomUserID = 
+const randomUserID = randomInteger + 1000000000
